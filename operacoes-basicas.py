79dc83a6a383081797b889f4c3e2a671b2df4170
Matheus Dutra <74274788+matheusdutraa@users.noreply.github.com>
2022-06-24 19:53:08 -0300

    Create operacoes-basicas.py

---
+++ operacoes-basicas.py
@@ -0,0 +1,96 @@
+# Soma
+
+s = "Somas"
+print(s)
+
+print(4 + 4)
+
+# Subtração
+print(6 - 3)
+
+# Multiplicação
+print(8 * 4)
+
+# Divisão
+print(20 / 7)
+
+# Potencia
+print(3 ** 4)
+
+# Módulo
+print(10 % 3)
+
+# --------------------- Função Type
+typee = "Função Type"
+
+print("\n", typee)
+
+print(type(5.0))
+
+print(type(5))
+
+a = 'Eu sou uma string.'
+
+print(type(a))
+
+# --------------------- Operações com N. float
+
+floats = 'Operações com números float'
+
+print("\n", floats)
+
+print(3.1 + 4.3)
+
+print(4 + 5.0)
+
+print(4 + 4)
+
+# Resultado é um número float
+print(4 / 2)
+
+# Resultado é um número inteiro
+print(4 // 2)
+
+print(4 / 3.0)
+
+print(4 // 3.0)
+
+# --------------------- Conversão
+
+c = "Conversão"
+
+print("\n", c)
+
+print(float(9))
+
+print(int(9.0))
+
+print(int(6.5))
+
+# --------------------- Hexadecimal e Binário
+
+hb = 'Hexadecimal e Binário'
+
+print("\n", hb)
+
+print(hex(394))
+
+print(bin(253))
+
+# --------------------- Funções abs, round e pow
+
+farp = 'Funções abs, round e pow'
+
+print("\n", farp)
+
+# Retorna o valor absoluto
+print(abs(-8))
+
+# Retorna o valor absoluto
+print(abs(8))
+
+# Retorna o valor com arrendondamento
+print(round(3.14151922,2))
+
+# Potencia
+print(pow(4,5))
