f728cc19cca75d8d1839b2aec5e02d64fb5c4117
Matheus Dutra <74274788+matheusdutraa@users.noreply.github.com>
2022-06-24 19:53:25 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,72 @@
+# --------------------- Variáveis e Operadores
+vo = "Variáveis e operadores"
+
+print("\n", vo)
+
+# Atribuindo o valor 1 á variável var_teste
+
+var_teste = 1
+
+# Imprimindo o valor da var_teste
+
+print(var_teste)
+
+# --------------------- Declaração Multipla
+
+mp = 'Declaração Multipla'
+
+print("\n", mp)
+
+pessoa1, pessoa2, pessoa3 = "Matheus", "Gabriel", "Thiago"
+
+print(pessoa1)
+
+print(pessoa2)
+
+print(pessoa3)
+
+fruta1 = fruta2 = fruta3 = "Manga"
+
+print(fruta1)
+
+print(fruta2)
+
+print(fruta3)
+
+"""
+Não se pode usar palavras reservadas como nome de variável
+False
+class
+finally
+is
+return
+None
+continue
+for
+lambda
+try
+True
+def
+from
+nonlocal
+while
+and
+del
+global
+not
+with
+as
+elif
+if
+or
+yield
+assert
+else
+import
+pass
+break
+except
+in
+raise
+"""
+
